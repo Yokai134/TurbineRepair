5b614e060e311170f5e1e5f88377e2977a06b092
Yokai134 <nyxny@DESKTOP-28674VR>
2023-04-07 00:24:52 +0500

    Добавьте файлы проекта.

---
+++ TurbineRepair/ViewModel/Base/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurbineRepair.ViewModel.Base
+{
+    internal class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChange([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChange(PropertyName); 
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+
+    }
+}
